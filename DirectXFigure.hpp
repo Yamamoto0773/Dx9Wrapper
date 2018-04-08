3c1febc3542f508de7cbf13c508b0c32dd431b7d
nanamin <nanamin0634@gmail.com>
2018-04-09 00:57:41 +0900

    add
    図形描画クラスのヘッダーファイル．とりあえずコミット

---
+++ DirectXFigure.hpp
@@ -0,0 +1,50 @@
+﻿#pragma once
+
+
+#include "Figure.hpp"
+
+namespace dx9 {
+
+	class DirectXFigure : public DXDrawManager {
+
+	private:
+		std::unique_ptr<EmptyTexture> emptyTex;
+	
+
+	public:
+		DirectXFigure();
+		~DirectXFigure();
+
+		bool Create();
+
+
+		// ----------------------------------------------
+		// 描画設定
+
+		// ----------------------------------------------
+		// 描画関数
+
+		// 直線描画
+		bool DrawLine(PointF begin, PointF end, DWORD color, float lineWidth=1.0f);
+
+		// 長方形の輪郭の描画
+		bool DrawRectFrame(RectF rect, DWORD color, float lineWidth=1.0f);
+		bool DrawRectFrame(float x, float y, float w, float h, DWORD color, float lineWidth=1.0f);
+
+		// 長方形の描画
+		bool DrawRect(RectF rect, DWORD color);
+		bool DrawRect(float x, float y, float w, float h, DWORD color);
+
+		// 円の輪郭の描画
+		bool DrawCircleFrame(float x, float y, float w, float h, DWORD color, float lineWidth=1.0f);
+
+		// 円の描画
+		bool DrawCircle(float x, float y, float w, DWORD color, float lineWidth=1.0f);
+
+
+
+	};
+
+
+
+}
