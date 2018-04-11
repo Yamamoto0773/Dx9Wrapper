3b53c18e534a091a4ae22fc8b344f592f7e9e75e
nanamin <nanamin0634@gmail.com>
2018-04-12 00:08:18 +0900

    add
    テンプレート関数の定義ファイル

---
+++ template.hpp
@@ -0,0 +1,26 @@
+﻿#pragma once
+
+
+#include <type_traits>
+
+namespace dx9 {
+
+
+	// 基底型にキャスト
+	template<class T> std::underlying_type<T> undering_cast(T a) {
+		return static_cast<std::underlying_type<T>>(a);
+	}
+
+
+	template<class T> int operator |(T L, T R) {
+		return undering_cast(L)|undering_cast(R);
+	}
+	template<class T> int operator &(T L, T R) {
+		return undering_cast(L)|undering_cast(R);
+	}
+
+
+}
+
+
+
