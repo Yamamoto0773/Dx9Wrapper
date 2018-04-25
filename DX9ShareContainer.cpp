580f48a55041e778da348301c67f6c1ed160ab7d
nanamin <nanamin0634@gmail.com>
2018-04-25 20:04:37 +0900

    add
    描画クラスで共有する変数を管理するDX9ShareContainerクラスを追加

---
+++ DX9ShareContainer.cpp
@@ -0,0 +1,26 @@
+﻿#include "DX9ShareContainer.hpp"
+
+namespace dx9 {
+
+	namespace resource {
+
+		bool									DX9ShareContainer::isResCreated = false;
+		size_t									DX9ShareContainer::topLayerPos = 0;
+		LogManager*								DX9ShareContainer::log;
+		CComPtr<IDirect3D9>						DX9ShareContainer::d3d9;
+		CComPtr<IDirect3DDevice9>				DX9ShareContainer::d3ddev9;
+		D3DCAPS9								DX9ShareContainer::d3dcaps9;
+		D3DPRESENT_PARAMETERS					DX9ShareContainer::d3dpresent;
+		CComPtr<IDirect3DVertexBuffer9>			DX9ShareContainer::vertex;
+		CComPtr<ID3DXEffect>					DX9ShareContainer::effect;		// シェーダ
+		CComPtr<IDirect3DVertexDeclaration9>	DX9ShareContainer::verDecl;	// 頂点宣言
+		D3DXMATRIX								DX9ShareContainer::projMat;
+		BLENDMODE								DX9ShareContainer::blendMode = BLENDMODE::NORMAL;
+		bool									DX9ShareContainer::isDrawStarted = false;
+		bool									DX9ShareContainer::isLost = false;
+		bool									DX9ShareContainer::isRightHand = false;
+		unsigned long							DX9ShareContainer::backGroundColor = 0xffffff;
+	}
+
+}
+
---
+++ DX9ShareContainer.hpp
@@ -0,0 +1,58 @@
+﻿#pragma once
+
+#include <d3dx9.h>
+#include <atlbase.h>
+
+#include "LogManager.hpp"
+#include "dx9.hpp"
+
+
+
+namespace dx9 {
+
+	namespace resource {
+
+		class DX9ShareContainer {
+		protected:
+
+			static bool isResCreated;
+			static size_t topLayerPos;
+
+			static CComPtr<IDirect3D9>			d3d9;
+			static CComPtr<IDirect3DDevice9>	d3ddev9;
+
+			static D3DCAPS9						d3dcaps9;
+
+			// DirectXを初期化する時に使う構造体
+			// どんな値を設定したか取っておいた方がいいので、メンバ変数とする。
+			static D3DPRESENT_PARAMETERS				d3dpresent;
+
+			static CComPtr<IDirect3DVertexBuffer9>		vertex;		// 頂点バッファ
+
+			static CComPtr<ID3DXEffect>					effect;		// シェーダ
+
+			static CComPtr<IDirect3DVertexDeclaration9>	verDecl;	// 頂点宣言
+
+
+			// 現在の状態を保持する変数
+			static D3DXMATRIX projMat;
+
+			static BLENDMODE blendMode;
+			static bool isDrawStarted;
+			static bool isLost;
+			static bool isRightHand;
+			static unsigned long backGroundColor;
+
+			static LogManager* log;
+
+			// 現在描画可能か返す
+			bool isDrawable() { return (isDrawStarted && (!isLost) && isResCreated); };
+
+		public:
+
+		};
+
+	}
+
+
+}
