9e673055bb2f54f45afe3e03dcfd1330cd1f819c
nanamin <nanamin0634@gmail.com>
2018-05-18 15:25:07 +0900

    add
    レンダリングターゲットリソースへの参照を保持するRenderingTargetクラスを追加

---
+++ RenderingTarget.cpp
@@ -0,0 +1,178 @@
+﻿#include "RenderingManager.hpp"
+
+
+namespace dx9 {
+
+	namespace renderer {
+
+
+
+
+		RenderingTarget_::RenderingTarget_() :
+			isValid(false),
+			index(0) {
+		}
+
+		RenderingTarget_::~RenderingTarget_() {
+		}
+
+
+		RenderingTarget RenderingTargetFactory::CreateRT(IDirect3DDevice9 * device, size_t w, size_t h) {
+			if (!device) nullptr;
+
+			int pos = searchEmptyRTPool();
+
+			if (pos != -1) {
+
+				RenderingTarget rt = std::unique_ptr<RenderingTarget_, RT_deleter_t>(new RenderingTarget_);
+
+				rt.get_deleter() = [this](RenderingTarget_ *rt) {
+					if (rt->isValid) {
+						RTpool[rt->index].user--;
+
+						if (RTpool[rt->index].user == 0) {
+
+							// Release resource
+							RTpool[rt->index].res.texSurface.Release();
+							RTpool[rt->index].res.depthStencilBuffer.Release();
+							RTpool[rt->index].res.texture.Delete();
+							ZeroMemory(&RTpool[rt->index].res.viewPort, sizeof(RTpool[rt->index].res.viewPort));
+							ZeroMemory(&RTpool[rt->index].surfaceDesc, sizeof(RTpool[rt->index].surfaceDesc));
+						}
+					}
+
+					delete rt;
+				};
+
+
+				// create RT-resources
+				if (!CreateRTContainer(device, w, h, RTpool[pos].res)) {
+					return nullptr;
+				}
+
+
+				RTpool[pos].user = 1;	// increment number of users
+				RTpool[pos].res.texSurface->GetDesc(&RTpool[pos].surfaceDesc);
+
+				rt->index = pos;		// set index for the RT-resource
+				rt->isValid = true;
+
+				return std::move(rt);
+			}
+
+			return nullptr;				// number of RT-resources has amounted to its max. 
+		}
+
+
+
+		void RenderingTargetFactory::ReleaseAll() {
+
+			for (auto &it : RTpool) {
+				if (it.user > 0) {
+
+					// Release All RT-resources
+					it.res.texSurface.Release();
+					it.res.depthStencilBuffer.Release();
+					it.res.texture.Delete();
+					ZeroMemory(&it.res.viewPort, sizeof(it.res.viewPort));
+
+					//logmng->tlnwrite("Release RT");
+				}
+			}
+
+		}
+
+		bool RenderingTargetFactory::Restore(IDirect3DDevice9 * device) {
+			if (!device) return false;
+
+			for (auto &it : RTpool) {
+				// Create All RT-resources
+				if (it.user > 0) {
+					if (!CreateRTContainer(device, it.surfaceDesc.Width, it.surfaceDesc.Height, it.res))
+						return false;
+				}
+			}
+
+			return true;
+		}
+
+
+		const RTContainer::Container* RenderingTargetFactory::GetContainer(const RenderingTarget_t &rt) {
+			if (!rt->isValid) return nullptr;
+
+			return &RTpool[rt->index].res;
+		}
+
+
+		int RenderingTargetFactory::searchEmptyRTPool() {
+
+			for (int i = 0; i < RTRESOURCE_MAXCNT; i++) {
+				if (RTpool[i].user == 0) return i;
+			}
+
+			return -1;
+		}
+
+
+		bool RenderingTargetFactory::CreateRTContainer(IDirect3DDevice9 *device, size_t w, size_t h, RTContainer::Container &container) {
+			if (!device) return false;
+			
+			HRESULT hr;
+
+			{
+				IDirect3DTexture9 *rawPtr;
+				if (FAILED(hr = device->CreateTexture(
+					w,
+					h,
+					1,
+					D3DUSAGE_RENDERTARGET,
+					D3DFMT_A8R8G8B8,
+					D3DPOOL_DEFAULT,
+					&rawPtr,
+					NULL
+					))) {
+					return false;
+				}
+				container.texture.Create(rawPtr);
+			}
+			{
+				IDirect3DSurface9 *rawPtr;
+				if (FAILED(hr = device->CreateDepthStencilSurface(
+					w,
+					h,
+					D3DFMT_D24S8,
+					D3DMULTISAMPLE_NONE,
+					0,
+					FALSE,
+					&rawPtr,
+					NULL
+					))) {
+					return false;
+				}
+				container.depthStencilBuffer.Attach(rawPtr);
+
+
+				if (FAILED(
+					container.texture.GetPointer()->GetSurfaceLevel(0, &rawPtr)
+					)) {
+					return false;
+				}
+				container.texSurface.Attach(rawPtr);
+
+
+				// set data for view-port
+				container.viewPort.Width = w;
+				container.viewPort.Height = h;
+				container.viewPort.X = 0;
+				container.viewPort.Y = 0;
+				container.viewPort.MinZ = 0.0f;
+				container.viewPort.MaxZ = 1.0f;
+			}
+			
+			return true;
+		}
+
+
+	}
+
+}
---
+++ RenderingTarget.hpp
@@ -0,0 +1,110 @@
+﻿#pragma once
+
+// (c) 2018, Nanami Yamamoto
+
+
+
+
+#include <d3d9.h>
+#include <atlbase.h>
+#include <array>
+#include <memory>
+#include <functional>
+
+#include "Singleton.hpp"
+#include "NonCopyable.hpp"
+
+#include "DXTextureBase.hpp"
+#include "LogManager.hpp"
+
+namespace dx9 {
+
+	namespace renderer {
+		class RenderingTarget_;
+		class RenderingTargetFactory;
+
+
+		constexpr unsigned RTRESOURCE_MAXCNT = 10;
+
+
+
+		struct RTContainer {
+			unsigned user;
+			D3DSURFACE_DESC surfaceDesc;
+			struct Container {
+				texture::DXTextureBase texture;
+				CComPtr<IDirect3DSurface9> texSurface, depthStencilBuffer;
+				D3DVIEWPORT9 viewPort;
+			}res;
+		};
+
+
+		// This class is used to reference Rendering Target maintained in its factory.
+		// User can manage ostensibly RT-resource with this class.
+		class RenderingTarget_ : Noncopyable {
+			friend class RenderingTargetFactory;
+
+		private:
+			RenderingTarget_();
+			~RenderingTarget_();
+			bool isValid;
+			unsigned index;
+		};
+
+
+
+		// This class manage to create and destroy Rendering Target.
+		class RenderingTargetFactory : public Singleton<RenderingTargetFactory> {
+			friend class Singleton<RenderingTargetFactory>;
+
+		public:
+			
+			using RT_deleter_t = std::function<void(RenderingTarget_*)>;
+			using RenderingTarget_t = std::unique_ptr<RenderingTarget_, RT_deleter_t>;
+			using RTpool_t = std::array<RTContainer, RTRESOURCE_MAXCNT>;
+
+		public:
+
+			// Create Rendering Target 
+			RenderingTarget_t CreateRT(IDirect3DDevice9 *device, size_t w, size_t h);
+
+			// Release all RT-resources created by user.
+			void ReleaseAll();
+
+			// Restore all RT-resources created by user.
+			bool Restore(IDirect3DDevice9 *device);
+
+			// Get RT-resources specified with [rt]. If [rt] is invalid, return nullptr.
+			const RTContainer::Container* GetContainer(const RenderingTarget_t &rt);
+
+
+		protected:
+			RenderingTargetFactory() {};
+			~RenderingTargetFactory() {};
+
+
+
+		private:
+			// search empty element in RTpool, and return index number indicated it.
+			// If not find it, return -1.
+			int searchEmptyRTPool();
+
+			
+			bool CreateRTContainer(
+				IDirect3DDevice9			*device,
+				size_t						w,
+				size_t						h,
+				RTContainer::Container		&container
+				);
+
+
+			// maintain RT-resource and number of user
+			std::array<RTContainer, RTRESOURCE_MAXCNT> RTpool;
+
+		};
+
+	
+	}
+
+	using RenderingTarget = renderer::RenderingTargetFactory::RenderingTarget_t;
+}
