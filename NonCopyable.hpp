f1842099b6c8fb50611afe9277cf35fb0846edc1
nanamin <nanamin0634@gmail.com>
2018-05-18 15:22:47 +0900

    add
    "Singleton"と"NonCopyable"のテンプレートクラスを追加

---
+++ NonCopyable.hpp
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+
+
+class Noncopyable {
+protected:
+	Noncopyable() {};
+	virtual ~Noncopyable() {};
+
+private:
+	void operator =(const Noncopyable& src) = delete; // copy is not permitted
+	Noncopyable(const Noncopyable& src) = delete;
+};
---
+++ Singleton.hpp
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+
+// This class is template class which provide "Singleton Pattern".
+// reference: https://qiita.com/narumi_/items/b205e59e7fc81695f380
+
+
+template<class T>
+class Singleton {
+public:
+	static inline T& GetInstance() {
+		static T instance;
+		return instance;
+	}
+
+protected:
+	Singleton() {};
+	virtual ~Singleton() {};
+
+private:
+	Singleton& operator=(const Singleton& obj) = delete;  // copy is not permitted
+	Singleton(const Singleton &obj) = delete; 
+};
+
