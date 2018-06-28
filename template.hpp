5ba5b61fbf66e629bab47e99fa6b117fcf54518a
山本七海 <nanamin0634@gmail.com>
2018-06-29 00:46:16 +0900

    fix
    C++14用に変更

--- template.hpp
+++ template.hpp
@@ -9,13 +9,13 @@ namespace dx9 {
 	// 基底型にキャスト
 	template<class T>
 	auto underlying_cast(T a) {
-		return static_cast<std::underlying_type<T>::type>(a);
+		return static_cast<typename std::underlying_type<T>::type>(a);
 	}
 
 	template<
 		class T1, class T2,
-		std::enable_if_t<std::is_enum<T1>::value, std::nullptr_t> = nullptr,
-		std::enable_if_t<std::is_enum<T2>::value, std::nullptr_t> = nullptr
+		typename std::enable_if_t<std::is_enum<T1>::value, std::nullptr_t> = nullptr,
+		typename std::enable_if_t<std::is_enum<T2>::value, std::nullptr_t> = nullptr
 	>
 	auto operator| (T1 L, T2 R) {
 		return underlying_cast(L)|underlying_cast(R);
@@ -24,7 +24,7 @@ namespace dx9 {
 
 	template<
 		class T1, class T2,
-		std::enable_if_t<std::is_enum<T1>::value, std::nullptr_t> = nullptr,
+		typename std::enable_if_t<std::is_enum<T1>::value, std::nullptr_t> = nullptr,
 		std::enable_if_t<std::is_enum<T2>::value, std::nullptr_t> = nullptr
 	>
 	auto operator& (T1 L, T2 R) {
