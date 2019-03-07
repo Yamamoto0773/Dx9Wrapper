69d41d2a39415d83369a17aaa23703190ea0a548
山本七海 <nanamin0634@gmail.com>
2019-03-07 23:12:24 +0900

    add a file for declaring directX interfaces and structures

---
+++ dx9interface.h
@@ -0,0 +1,19 @@
+#pragma once
+
+
+// fowarding declaration
+
+struct IDirect3DDevice9;
+struct IDirect3D9;
+struct IDirect3DTexture9;
+struct IDirect3DVertexBuffer9;
+struct ID3DXEffect;
+struct IDirect3DVertexDeclaration9;
+struct IDirect3DSurface9;
+
+typedef struct _D3DCAPS9 D3DCAPS9;
+typedef struct _D3DPRESENT_PARAMETERS_ D3DPRESENT_PARAMETERS;
+typedef struct _D3DSURFACE_DESC D3DSURFACE_DESC;
+typedef struct _D3DVIEWPORT9 D3DVIEWPORT9;
+typedef struct D3DXMATRIX D3DXMATRIX;
+typedef struct _D3DLOCKED_RECT D3DLOCKED_RECT;
